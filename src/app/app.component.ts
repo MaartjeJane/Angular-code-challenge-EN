47241af5f788a12433f39a19fc2315f23d5e5b32
Maartje O'Connor <maartje.oconnor@achmea.nl>
2025-04-14 09:05:54 +0200

    Assessment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { KentekenCheck } from 'rdw-kenteken-check';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  vehicleType: string = 'auto';
+  validLicense: boolean = true;
+  licensePlate: string = '';
+
+  onVehicleSelect(event: any) {
+    this.vehicleType = event.target.value; 
+  }
+
+  validateLicensePlate(event: any) {
+    if (event.target.value) {
+      const kentekenCheck = new KentekenCheck(event.target.value);
+      const formattedLicensePlate = kentekenCheck.formatLicense();
+
+      if (formattedLicensePlate === 'XX-XX-XX') {
+        this.validLicense = false;
+      } else {
+        this.validLicense = true;
+        this.licensePlate = formattedLicensePlate;
+      }
+    }
+  }
 }
